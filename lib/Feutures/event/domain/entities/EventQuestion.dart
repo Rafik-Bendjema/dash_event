bd1cfd92a144194e68b37f326668d86fecbee82d
Rafik-Bendjema <bendjema.rafik3@gmail.com>
2023-12-22 22:16:21 +0100

    ask dash

---
+++ lib/Feutures/event/domain/entities/EventQuestion.dart
@@ -0,0 +1,9 @@
+class EventQuestion {
+  final String question;
+  final String answer;
+  EventQuestion({required this.question, required this.answer});
+
+  List<EventQuestion> fromJson(Map<String, dynamic> json) {
+    return [];
+  }
+}
--- lib/Feutures/event/presentation/pages/EvnetPage.dart
+++ lib/Feutures/event/presentation/pages/EvnetPage.dart
@@ -2,6 +2,8 @@ import 'package:dash_event/Feutures/event/presentation/widgets/CountDown.dart';
 import 'package:dash_event/Feutures/event/presentation/widgets/NotificationField.dart';
 import 'package:flutter/material.dart';
 
+bool isDark = false;
+
 class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
@@ -13,26 +15,60 @@ class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor:
+          isDark ? const Color.fromARGB(255, 43, 43, 43) : Colors.white,
       body: SafeArea(
         child: Column(children: [
           Align(
             alignment: Alignment.centerRight,
             child: Padding(
               padding: const EdgeInsets.only(right: 20, top: 10),
-              child: Container(
-                child: Center(
-                  child: Container(
-                    width: 25,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isDark = !isDark;
+                  });
+                },
+                child: Stack(children: [
+                  Container(
+                    height: 50,
+                    width: 100,
+                    //  padding: EdgeInsets.symmetric(vertical: 10),
                     decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.yellow),
+                        color: isDark
+                            ? Colors.black
+                            : Color.fromARGB(255, 145, 140, 140),
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
                   ),
-                ),
-                height: 50,
-                width: 100,
-                //  padding: EdgeInsets.symmetric(vertical: 10),
-                decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 179, 179, 179),
-                    borderRadius: BorderRadius.all(Radius.circular(50))),
+                  AnimatedPositioned(
+                    duration: const Duration(milliseconds: 200),
+                    width: 25,
+                    height: 25,
+                    top: 11,
+                    left: isDark ? 50 : 5,
+                    child: Stack(clipBehavior: Clip.none, children: [
+                      Positioned(
+                        width: 25,
+                        height: 25,
+                        left: 10,
+                        child: Container(
+                          width: 25,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle, color: Colors.yellow),
+                        ),
+                      ),
+                      Positioned(
+                        child: Container(
+                          width: 25,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  isDark ? Colors.black : Colors.transparent),
+                        ),
+                      )
+                    ]),
+                  ),
+                ]),
               ),
             ),
           ),
@@ -54,7 +90,11 @@ class _EventPageState extends State<EventPage> {
               ),
               Text(
                 "is starting the event in",
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(
+                    fontSize: 20,
+                    color: isDark
+                        ? const Color.fromARGB(255, 197, 197, 197)
+                        : const Color.fromARGB(255, 63, 63, 63)),
               )
             ],
           ),
