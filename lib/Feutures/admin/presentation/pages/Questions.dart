a308e0877d382ad2da92268b7ea5bae2704d8aad
Rafik-Bendjema <bendjema.rafik3@gmail.com>
2023-12-24 19:10:40 +0100

    questions page

--- lib/Feutures/admin/presentation/pages/Questions.dart
+++ lib/Feutures/admin/presentation/pages/Questions.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dash_event/Feutures/admin/presentation/widgets/AddQuestion.dart';
+import 'package:dash_event/Feutures/event/domain/entities/EventQuestion.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 class Questions extends StatefulWidget {
@@ -8,8 +12,107 @@ class Questions extends StatefulWidget {
 }
 
 class _QuestionsState extends State<Questions> {
+  List<EventQuestion> questions = [];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    for (var i = 0; i < 20; i++) {
+      questions.add(EventQuestion(
+          question: faker.person.name(), answer: faker.person.name()));
+    }
+
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Questions",
+                  style: TextStyle(fontSize: 30),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Image.asset(
+                  "assets/images/dash3.png",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => const Dialog(
+                                child: AddQuestion(),
+                              ));
+                    },
+                    child: const Text("add"),
+                  ),
+                ),
+              ),
+              ListView.builder(
+                  physics:
+                      const NeverScrollableScrollPhysics(), // Disable scrolling
+                  shrinkWrap: true,
+                  itemCount: questions.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: Color.fromARGB(255, 224, 224, 224)),
+                          padding: const EdgeInsets.all(5),
+                          width: MediaQuery.of(context).size.width * 0.80,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  questions[index].question,
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    showDialog(
+                                        context: context,
+                                        builder: (context) => Dialog(
+                                              child: SizedBox(
+                                                width: 300,
+                                                height: 300,
+                                                child: Column(children: [
+                                                  Text(questions[index]
+                                                      .question),
+                                                  Text(questions[index].answer)
+                                                ]),
+                                              ),
+                                            ));
+                                  },
+                                  icon: const Icon(Icons.info))
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        )
+                      ],
+                    );
+                  })
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
